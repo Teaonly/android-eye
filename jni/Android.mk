d157504ee4b24c31d8b46eb19e2bcbedb3c17dce
Zhou Chang <chang.zhou@intel.com>
2012-11-01 16:43:27 +0800

    Added uPnP port mapping native code supporting.

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,17 +5,29 @@
 LOCAL_PATH:= $(call my-dir)
 
 ###########################################################
-# build android libteaony 
-# libteaonly: clone from libjingle, for generat NDK appplicaton
-#      threads, socket, signal/slot and messages, etc.
+#   mp3 encoder
 #
 include $(CLEAR_VARS)
 LOCAL_MODULE := libmp3encoder
 LOCAL_CFLAGS := -O2 -Wall -DANDROID -DSTDC_HEADERS -I./libmp3lame/ 
 
 #including source files
-include $(LOCAL_PATH)/lib_build.mk
+include $(LOCAL_PATH)/libmp3lame_build.mk
 
 LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)
+
+###########################################################
+#   uPnP port mapping 
+#
+include $(CLEAR_VARS)
+LOCAL_MODULE := libnatpmp
+LOCAL_CFLAGS := -O2 -Wall -DANDROID -DLINUX -I./libnatpmp/  
+
+#including source files
+include $(LOCAL_PATH)/libnatpmp_build.mk
+
+LOCAL_LDLIBS := -llog
+
+include $(BUILD_SHARED_LIBRARY)   
